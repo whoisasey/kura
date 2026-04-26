764d19195308120fd34cefa666cac749b7bcddb9
atina <56001902+whoisasey@users.noreply.github.com>
2026-04-25 21:00:28 -0400

    update loading state (#1)
    * update loading state

---
+++ src/app/login/loading.tsx
@@ -0,0 +1,18 @@
+import { Box, CircularProgress } from "@mui/material";
+
+const LoginLoading = () => {
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
+};
+
+export default LoginLoading;
